e5ec2aebd4d34c95ddadebd77cabbbab977348f5
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-02-21 16:55:54 +0100

    add a game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,24 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.user = current_user
+    if @game.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description, :address, :price, :min_player, :max_player)
+  end
 end

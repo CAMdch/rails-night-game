e11c6261438d89c563132726f20ca043a4cfed9f
Emily Haynes <emi.haynes@icloud.com>
2022-02-22 14:42:25 +0100

    Changed the form and made it mandatory to add a picture

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,8 @@ class Game < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  has_one_attached :photo
+  has_one_attached :photo, dependent: :destroy
 
-  validates :description, :name, :price, :address, :min_player, :max_player, :playtime, presence: true
+  validates :description, :name, :price, :address, :min_player, :max_player, :playtime, :photo, presence: true
   validates :min_player, numericality: { greater_than: 0, less_than_or_equal_to: :max_player }
 end

1ff21d7565c60c55065389a7aa668ae8feebf1b6
Emily Haynes <emi.haynes@icloud.com>
2022-02-22 15:26:19 +0100

    added limit the name must be btw 3 and 60 characters long

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,5 +5,6 @@ class Game < ApplicationRecord
   has_one_attached :photo, dependent: :destroy
 
   validates :description, :name, :price, :address, :min_player, :max_player, :playtime, :photo, presence: true
+  validates :name, length: { in: 3..60 }
   validates :min_player, numericality: { greater_than: 0, less_than_or_equal_to: :max_player }
 end

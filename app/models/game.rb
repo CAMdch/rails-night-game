2fd4b52e3271a82a3d5af4a2665070105daa8471
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-02-22 09:58:33 +0100

    validates min max player

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,5 @@ class Game < ApplicationRecord
   has_many :users, through: :bookings
 
   validates :description, :name, :price, :address, :min_player, :max_player, :playtime, presence: true
+  validates :min_player, numericality: { greater_than: 0, less_than_or_equal_to: :max_player }
 end

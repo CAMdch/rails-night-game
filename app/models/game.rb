0f18e6409cc9063c745e7aacb5f99a3eea3e5f06
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-02-22 13:12:55 +0100

    profil controller

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
 
   validates :description, :name, :price, :address, :min_player, :max_player, :playtime, presence: true

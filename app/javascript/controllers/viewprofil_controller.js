e39fd6e940a7e3997ba6a7d86d2676bbde1e5eba
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-02-22 17:35:55 +0100

    view bouton progil

--- app/javascript/controllers/viewprofil_controller.js
+++ app/javascript/controllers/viewprofil_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["products", "bookings"]
+  static targets = ["products", "bookings", "btnproducts", "btnbookings"]
 
   connect() {
     console.log(this.productsTarget)
@@ -13,9 +13,17 @@ export default class extends Controller {
     if (event.currentTarget.dataset.choice === "products") {
       this.productsTarget.classList.remove("d-none")
       this.bookingsTarget.classList.add("d-none")
+      this.btnproductsTarget.classList.remove("btn-unactive")
+      this.btnproductsTarget.classList.add("btn-active")
+      this.btnbookingsTarget.classList.remove("btn-active")
+      this.btnbookingsTarget.classList.add("btn-unactive")
     } else if (event.currentTarget.dataset.choice === "bookings") {
       this.bookingsTarget.classList.remove("d-none")
       this.productsTarget.classList.add("d-none")
+      this.btnbookingsTarget.classList.remove("btn-unactive")
+      this.btnbookingsTarget.classList.add("btn-active")
+      this.btnproductsTarget.classList.remove("btn-active")
+      this.btnproductsTarget.classList.add("btn-unactive")
     }
   }
 }

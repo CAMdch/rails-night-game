6428e9289a85e9021821dac6545030ef35823cb0
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-02-22 14:56:06 +0100

    css added

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,21 +22,20 @@ puts "Delete all users"
 User.destroy_all
 
 puts "Creating users"
-alexis = User.create( name: "Alexis Sion", email: "alexis@gmail.com", phone: "0611223344", password: "azerty")
-camille = User.create( name: "Camille Dauchy", email: "camille@gmail.com", phone: "0622334455", password: "azerty")
-emily = User.create( name: "Emily Haynes", email: "emily@gmail.com", phone: "0633445566", password: "azerty")
+alexis = User.create(name: "Alexis Sion", email: "alexis@gmail.com", phone: "0611223344", password: "azerty")
+camille = User.create(name: "Camille Dauchy", email: "camille@gmail.com", phone: "0622334455", password: "azerty")
+emily = User.create(name: "Emily Haynes", email: "emily@gmail.com", phone: "0633445566", password: "azerty")
 
 puts "#{User.count} User Created !"
 
 puts "Creating games"
-root = Game.create( name: "Root", description: "Find adventure in this marvelous asymmetric game.", price: "5€", min_player:"2", max_player:"4" , playtime:"60min" , address: "Lille" , user: alexis )
-scythe = Game.create( name: "Scythe", description: "Scythe gives players almost complete control over their fate. ", price: "4€", min_player:"1", max_player: "5", playtime: "90min", address:"Lomme", user: alexis)
-terraforming_mars = Game.create( name: "Terraforming Mars", description: "In the 2400s, mankind begins to terraform the planet Mars.", price: "6€", min_player:"2", max_player: "5", playtime: "90min", address: "Mouvaux" , user: alexis)
-pandemic = Game.create( name: "Pandemic", description: "After five years of Pandemic, hundreds of thousands of players have contracted the virus!", price: "3€", min_player:"2", max_player: "4", playtime: "45min", address: "Croix" , user: camille)
-wingspan = Game.create( name: "Wingspan", description: "Wingspan is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games.", price: "5€", min_player:"2", max_player:"4" , playtime:"40min" , address: "Lille" , user: camille )
+root = Game.create(name: "Root", description: "Find adventure in this marvelous asymmetric game.", price: "5€", min_player:"2", max_player:"4" , playtime:"60min" , address: "Lille" , user: alexis )
+scythe = Game.create(name: "Scythe", description: "Scythe gives players almost complete control over their fate. ", price: "4€", min_player:"1", max_player: "5", playtime: "90min", address:"Lomme", user: alexis)
+terraforming_mars = Game.create(name: "Terraforming Mars", description: "In the 2400s, mankind begins to terraform the planet Mars.", price: "6€", min_player:"2", max_player: "5", playtime: "90min", address: "Mouvaux" , user: alexis)
+pandemic = Game.create(name: "Pandemic", description: "After five years of Pandemic, hundreds of thousands of players have contracted the virus!", price: "3€", min_player:"2", max_player: "4", playtime: "45min", address: "Croix" , user: camille)
+wingspan = Game.create(name: "Wingspan", description: "Wingspan is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games.", price: "5€", min_player:"2", max_player:"4" , playtime:"40min" , address: "Lille" , user: camille )
 
 puts " #{Game.count}Game Created !"
-
 
 booking_1 = Booking.create!(user: emily, game: root, date_begin: Date.today,date_end: Date.today + 3)
 booking_2 = Booking.create!(user: emily, game: pandemic, date_begin: Date.today + 5,date_end: Date.today + 9)

d8c8d7aa36ac2c1350860c3f05673a4c0cf6bc9b
Alexis Sion <asion2000@gmail.com>
2022-02-23 16:23:58 +0100

    add picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :bookings
   has_many :games, through: :bookings
   has_many :reviews
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable

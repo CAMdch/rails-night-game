4edfc52cea85ea546e3cd792c4b7f5223f134157
Camille Dauchy <camille.dauchy@2020.icam.fr>
2022-02-24 21:34:06 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,32 +44,32 @@ puts "#{User.count} User Created !"
 puts "Creating games"
 
 photo_root = URI.open('https://d2k4q26owzy373.cloudfront.net/350x350/games/uploaded/1629324760985.jpg')
-root = Game.new( name: "Root", description: "Find adventure in this marvelous asymmetric game.", price: "5€", min_player:"2", max_player:"4" , playtime:"60min" , address: "Lille" , user: alexis )
+root = Game.new( name: "Root", description: "Find adventure in this marvelous asymmetric game.", price: "5€", min_player:"2", max_player:"4" , playtime:"60min" , address: "2 Avenue Des Saules, 59160 Lille, France" , user: alexis )
 root.photos.attach(io: photo_root, filename: 'root-image.jpg', content_type: 'image/jpg')
 root.save!
 
 photo_scythe = URI.open('https://cdn.shopify.com/s/files/1/0513/4077/1515/products/scythe-board-game.jpg')
-scythe = Game.new( name: "Scythe", description: "Scythe gives players almost complete control over their fate. ", price: "4€", min_player:"1", max_player: "5", playtime: "90min", address:"Lomme", user: alexis)
+scythe = Game.new( name: "Scythe", description: "Scythe gives players almost complete control over their fate. ", price: "4€", min_player:"1", max_player: "5", playtime: "90min", address:"Rue De Wattiessart, 59113 Seclin, France", user: alexis)
 scythe.photos.attach(io: photo_scythe, filename: 'scythe-image.jpg', content_type: 'image/jpg')
 scythe.save!
 
 photo_terraforming_mars = URI.open('https://d2k4q26owzy373.cloudfront.net/350x350/games/uploaded/1629324032557.jpg')
-terraforming_mars = Game.new( name: "Terraforming Mars", description: "In the 2400s, mankind begins to terraform the planet Mars.", price: "6€", min_player:"2", max_player: "5", playtime: "90min", address: "Mouvaux" , user: alexis)
+terraforming_mars = Game.new( name: "Terraforming Mars", description: "In the 2400s, mankind begins to terraform the planet Mars.", price: "6€", min_player:"2", max_player: "5", playtime: "90min", address: "Avenue De La République, 59700 Marcq-en-Barœul, France" , user: alexis)
 terraforming_mars.photos.attach(io: photo_terraforming_mars, filename: 'terraforming_mars-image.jpg', content_type: 'image/jpg')
 terraforming_mars.save!
 
 photo_pandemic = URI.open('https://d2k4q26owzy373.cloudfront.net/350x350/games/uploaded/1559254186140-51iNoyxoamL.jpg')
-pandemic = Game.new( name: "Pandemic", description: "After five years of Pandemic, hundreds of thousands of players have contracted the virus!", price: "3€", min_player:"2", max_player: "4", playtime: "45min", address: "Croix" , user: camille)
+pandemic = Game.new( name: "Pandemic", description: "After five years of Pandemic, hundreds of thousands of players have contracted the virus!", price: "3€", min_player:"2", max_player: "4", playtime: "45min", address: "Rue Faidherbe, 59139 Wattignies, France" , user: camille)
 pandemic.photos.attach(io: photo_pandemic, filename: 'pandemic-image.jpg', content_type: 'image/jpg')
 pandemic.save!
 
 photo_wingspan = URI.open('https://d2k4q26owzy373.cloudfront.net/350x350/games/uploaded/1629325193747.png')
-wingspan = Game.new( name: "Wingspan", description: "Wingspan is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games.", price: "5€", min_player:"2", max_player:"4" , playtime:"40min" , address: "Lille" , user: camille )
+wingspan = Game.new( name: "Wingspan", description: "Wingspan is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games.", price: "5€", min_player:"2", max_player:"4" , playtime:"40min" , address: "2 Rue Auber, 59800 Lille, France" , user: camille )
 wingspan.photos.attach(io: photo_wingspan, filename: 'wingspan-image.png', content_type: 'image/png')
 wingspan.save!
 
 photo_viticulture = URI.open('https://d2k4q26owzy373.cloudfront.net/350x350/games/uploaded/1629323034692.jpg')
-viticulture = Game.new( name: "Viticulture", description: "Viticulture is a worker-placement game set in the foothills of Tuscany. ", price: "7€", min_player:"1", max_player:"6" , playtime:"120min" , address: "Lille" , user: camille )
+viticulture = Game.new( name: "Viticulture", description: "Viticulture is a worker-placement game set in the foothills of Tuscany. ", price: "7€", min_player:"1", max_player:"6" , playtime:"120min" , address: "Rue Royale, 59800 Lille, France" , user: camille )
 viticulture.photos.attach(io: photo_viticulture, filename: 'viticulture-image.jpg', content_type: 'image/jpg')
 viticulture.save!
 
